6286e385b1d9d426642b92751cb9ef6602d1c3b3
abileneElaine <145374967+abileneElaine@users.noreply.github.com>
2024-08-19 13:47:38 -0300

    aula08

--- Oficina2024/Assets/Scripts/Cherrie.cs
+++ Oficina2024/Assets/Scripts/Cherrie.cs
@@ -27,6 +27,7 @@ public class Cherrie : MonoBehaviour
             collected.SetActive(true);
 
             GameController.instance.totalScore += Score;
+            GameController.instance.UpdateScoreText();
             
             Destroy(gameObject , 0.25f);
         }
